62af71e0fc398c124d9cdf6440eaa7445e4148da
yuya yano <yuya.yano.6260@gmail.com>
2014-04-21 14:56:21 +0900

    Added a controller sample.

--- static/js/general.js
+++ static/js/general.js
@@ -4,3 +4,32 @@ app.config(function($interpolateProvider) {
   $interpolateProvider.startSymbol('[[');
   $interpolateProvider.endSymbol(']]');
 });
+
+
+// Controller section
+function TodoCtrl($scope) {
+	$scope.todos = [
+		{text:"hoge", done:true},
+		{text:"hige", done:false}];
+
+	$scope.addTodo = function(){
+		$scope.todos.push({text:$scope.todoText, done:false});
+		$scope.todoText = '';
+	};
+
+	$scope.remaining = function(){
+		var count = 0;
+		angular.forEach($scope.todos, function(todo) {
+			count += todo.done ? 0 : 1;
+		});
+		return count;
+	};
+
+	$scope.archive = function(){
+		var oldTodos = $scope.todos;
+		$scope.todos = [];
+		angular.forEach(oldTodos, function(todo) {
+			if (!todo.done) $scope.todos.push(todo);
+		});
+	};
+}
